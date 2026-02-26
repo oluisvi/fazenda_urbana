0e954623fe44b202f99058efd09f41ad37ab7bf1
oluisvi <oluisvipersonal@gmail.com>
2026-02-25 22:27:32 -0300

    upload - mandando arquivos realocados para repositório

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "pim-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/dashboard.html",
+  "/style.css",
+  "/script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
